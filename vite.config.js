a9e3caa3c8b93b867042d33437d4e80a6fc27b5c
Titus kagina <kaginatitus@gmail.com>
2026-04-13 11:47:27 +0300

    Fix deployment: Added GitHub Action, set relative base path, and cleaned up asset linking

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/nancynjugunaconsultancy/',
+  base: './',
   build: {
     rollupOptions: {
       input: {
